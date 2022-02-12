7058d217eca6a084c53df8829258308600da3bb0
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-02-13 03:21:15 +0530

    now scrapes amazon as well, initial stage

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ def extract_info(soup):
     return extracted
 
 
-req = requests.get("https://www.flipkart.com/apple-iphone-12-black-64-gb/p/itma2559422bf7c7?pid=MOBFWBYZU5FWK2VP&lid=LSTMOBFWBYZU5FWK2VPUYA8BN&marketplace=FLIPKART&q=iphone+12&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_1_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_1_na_na_na&fm=search-autosuggest&iid=8b500cf8-05af-491e-8626-47669c079861.MOBFWBYZU5FWK2VP.SEARCH&ppt=hp&ppn=homepage&ssid=l6jqth7j5c0000001644666303046&qH=7b7504afcaf2e1ea")
-if(req.status_code == 200):
-    soup = BeautifulSoup(req.text, "html.parser")
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+url = "https://www.amazon.in/New-Apple-iPhone-12-64GB/dp/B08L5VJYV7/ref=sr_1_3?keywords=iphone+12&qid=1644702135&sprefix=iphone%2Caps%2C236&sr=8-3"
+url2 = "https://www.amazon.in/Kurkure-Green-Chutney-Rajasthani-Style/dp/B009GIUZNE?pd_rd_w=BCBky&pf_rd_p=ace64564-89b9-4863-a50c-f8acef1159bb&pf_rd_r=SDR83NSMMK2KYH6TAMDY&pd_rd_r=5b5b65b4-f307-43e2-a389-5baeda32daf5&pd_rd_wg=3MQRr&psc=1&ref_=pd_bap_d_rp_18_i"
+req = requests.get(url=url2, headers=header)
+soup = BeautifulSoup(req.text, "html.parser")
+print(soup.find(class_="a-offscreen"))
 
-    extract_tuple = extract_info(soup)
-    product_title = extract_tuple.title
-    product_price = extract_tuple.price
+# extract_tuple = extract_info(soup)
+# product_title = extract_tuple.title
+# product_price = extract_tuple.price
 
-    with open("data.csv", "w") as csvfile:
-        csv_writer = writer(csvfile)
-        csv_writer.writerow([product_title, product_price])
-else:
-    print("Server issues!!")
+# with open("data.csv", "w") as csvfile:
+#     csv_writer = writer(csvfile)
+#     csv_writer.writerow([product_title, product_price])
