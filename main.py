685bed5b7ad52b7427e733ec43e35cc5967ae3a1
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-02-14 01:27:30 +0530

    Updated code (#1)
    * now scrapes amazon as well, initial stage
    
    * Updated code

--- main.py
+++ main.py
@@ -2,27 +2,58 @@ from bs4 import BeautifulSoup
 import requests
 from collections import namedtuple
 from csv import reader, writer
+import re
+
+data = namedtuple("data", ["title", "price"])
 
 
-def extract_info(soup):
-    data = namedtuple("data", ["title", "price"])
+def extract_flipkart_info(soup):
     container = soup.find(
         id="container").contents[0].contents[2].contents[0].contents[1].contents[1].contents[-1].contents
     extracted = data(
-        container[0].text, container[-1].contents[0].contents[0].text.split("₹")[1])
+        container[0].text.strip(), container[-1].contents[0].contents[0].text.split("₹")[1].strip())
     return extracted
 
 
-req = requests.get("https://www.flipkart.com/apple-iphone-12-black-64-gb/p/itma2559422bf7c7?pid=MOBFWBYZU5FWK2VP&lid=LSTMOBFWBYZU5FWK2VPUYA8BN&marketplace=FLIPKART&q=iphone+12&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_1_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_1_na_na_na&fm=search-autosuggest&iid=8b500cf8-05af-491e-8626-47669c079861.MOBFWBYZU5FWK2VP.SEARCH&ppt=hp&ppn=homepage&ssid=l6jqth7j5c0000001644666303046&qH=7b7504afcaf2e1ea")
+def extract_amazon_info(soup):
+    extracted = soup.find(attrs={"data-a-color": "price"}).text
+    product_price = extracted.split(".")[0]
+    if(product_price[0] == "₹"):
+        product_price = product_price[1:]
+    product_title = soup.find(id="productTitle").text
+    extracted_data = data(product_title.strip(), product_price.strip())
+    return extracted_data
+
+
+def is_amazon(link):
+    amazon = re.search(r'https?://www.amazon.in', link)
+    if(amazon is None):
+        return False
+    return True
+
+
+def is_flipkart(link):
+    flipkart = re.search(r'https?://www.flipkart.com', link)
+    if(flipkart is None):
+        return False
+    return True
+
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+url = input("Enter url: ")
+if(not is_amazon(url) and not is_flipkart(url)):
+    print("Not a valid url")
+    exit()
+req = requests.get(url=url, headers=header)
 if(req.status_code == 200):
     soup = BeautifulSoup(req.text, "html.parser")
-
-    extract_tuple = extract_info(soup)
-    product_title = extract_tuple.title
-    product_price = extract_tuple.price
-
-    with open("data.csv", "w") as csvfile:
-        csv_writer = writer(csvfile)
-        csv_writer.writerow([product_title, product_price])
+    if(is_amazon(url)):
+        extracted_data = extract_amazon_info(soup)
+    elif(is_flipkart(url)):
+        extracted_data = extract_flipkart_info(soup)
+    print(extracted_data.title, extracted_data.price)
 else:
-    print("Server issues!!")
+    print("Server issues!!, Data can't be retrieved. Sorry!!")
