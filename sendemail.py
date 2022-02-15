70153c88be51c2f80e05c55bf2212c0eb1290e57
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-02-16 03:58:15 +0530

    Added files and packages

---
+++ sendemail.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import smtplib
+
+load_dotenv()
+
+EMAILID = os.getenv("EMAILID")
+PASSWORD = os.getenv("PASSWORD")
+SERVER = os.getenv("server")
+
+
+def send_email(reciever_email, extracted, url):
+    product = extracted.title
+    price = extracted.price
+    connection = smtplib.SMTP(SERVER)
+    connection.starttls()
+    connection.login(user=EMAILID, password=PASSWORD)
+    connection.sendmail(from_addr=EMAILID, to_addrs=reciever_email,
+                        msg=f"Subject:PRODUCT {product} available at a cheaper price!!\n\nHey there,\nThe product you have been keeping an eye on {product} is now available at a price of Rs{price}\nGo ahead and grab it at {url}")
+    connection.close()
